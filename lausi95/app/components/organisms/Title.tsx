f81a5f6c255b236b75deb20510cd91f266dbb52c
Tom Lausmann <tom.lausmann@adesso.de>
2024-09-10 22:19:16 +0200

    feat: add logo (not nice, yet)

--- lausi95/app/components/organisms/Title.tsx
+++ lausi95/app/components/organisms/Title.tsx
@@ -1,12 +1,19 @@
+import Image from 'next/image';
+
 export type PageTitleProps = {
     subtitle?: string;
 };
 
 export default function PageTitle({ subtitle }: PageTitleProps) {
     return (
-        <div className="p-4 bg-[#010409] text-sm">
-            <div className="text-gray-500">Tom Lausmann</div>
-            <div className="font-bold">{subtitle ?? 'Unknown page'}</div>
+        <div className="p-4 bg-[#010409] text-sm flex gap-4">
+            <div>
+                <Image src="/tl95.svg" alt="Logo" height={40} width={40} />
+            </div>
+            <div className="flex flex-col content-center justify-center">
+                <div className="text-gray-400">Tom Lausmann</div>
+                <div className="font-bold">{subtitle ?? 'Unknown page'}</div>
+            </div>
         </div>
     );
 }
