ba80beace8d670f6163a0cfdcf5f30e7707e8bb2
Tom Lausmann <tom.lausmann@adesso.de>
2024-09-10 22:47:31 +0200

    feat: new logo, adjust header sizes

--- lausi95/app/components/organisms/Title.tsx
+++ lausi95/app/components/organisms/Title.tsx
@@ -6,9 +6,9 @@ export type PageTitleProps = {
 
 export default function PageTitle({ subtitle }: PageTitleProps) {
     return (
-        <div className="p-4 bg-[#010409] text-sm flex gap-4">
+        <div className="p-4 bg-[#010409] text-xl flex gap-4">
             <div>
-                <Image src="/tl95.svg" alt="Logo" height={40} width={40} />
+                <Image src="/tl95.svg" alt="Logo" height={60} width={60} />
             </div>
             <div className="flex flex-col content-center justify-center">
                 <div className="text-gray-400">Tom Lausmann</div>
