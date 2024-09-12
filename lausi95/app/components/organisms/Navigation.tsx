cda41554a631e1a83c37bb0917e15c0ac06a23b4
Tom Lausmann <tom.lausmann@adesso.de>
2024-09-12 14:39:00 +0200

    feat: introduce icons for navigation

--- lausi95/app/components/organisms/Navigation.tsx
+++ lausi95/app/components/organisms/Navigation.tsx
@@ -1,3 +1,5 @@
+import { faBook, faHouse, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export type NavPage = "home" | "blog";
@@ -10,6 +12,7 @@ type NavItem = {
     text: string;
     href: string;
     active: boolean;
+    icon: IconDefinition;
 };
 
 export default function Navigation({ page }: NavigationProps) {
@@ -18,11 +21,13 @@ export default function Navigation({ page }: NavigationProps) {
             text: 'Home',
             href: '/',
             active: page == 'home',
+            icon: faHouse,
         },
         {
             text: 'Blog',
             href: '/blog',
             active: page == 'blog',
+            icon: faBook,
         },
     ];
 
@@ -30,7 +35,14 @@ export default function Navigation({ page }: NavigationProps) {
         <div className="flex bg-[#010409]">
             <div className="p-2 border-b border-gray-700"></div>
             {items.map(item => (
-                <Link key={item.href} className={item.active ? 'p-2 border-b-2 border-emerald-300 font-bold' : 'p-2 border-b border-gray-700'} href={item.href}>{item.text}</Link>
+                <Link key={item.href}
+                    className={'px-4 py-2 border-b-2 ' + (item.active ? 'border-emerald-300 font-bold' : 'border-gray-700')}
+                    href={item.href}>
+                    <div className="flex justify-center items-center gap-4">
+                        <div><FontAwesomeIcon icon={item.icon} className="h-[20px]" /></div>
+                        <div>{item.text}</div>
+                    </div>
+                </Link>
             ))}
             <div className="grow border-b border-gray-700"></div>
         </div>
