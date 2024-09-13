f138db71286160cad4ad905c998d71e548737776
Tom Lausmann <tom.lausmann@adesso.de>
2024-09-13 10:50:07 +0200

    feat: add Block element to space things nicer

--- lausi95/app/page.tsx
+++ lausi95/app/page.tsx
@@ -1,23 +1,26 @@
-import { promises as fs } from 'fs';
 import CodeSample from "./components/molecules/CodeSample";
 import BasicPage from "./components/templates/BasicPage";
+import Block from "./components/atoms/Block";
 
 export default async function Home() {
     return (
         <BasicPage page="home" subtitle="Home">
-            <p>
-                Hello! My Name is Tom Lausmann and this is going to be my personal
-                Website. Here, you will find everything that I want you to know about
-                me as a person, my hobbies and personal projects.
-            </p>
-            <p className="font-bold">
+            <Block paragraph>
+                Hello! My Name is Tom Lausmann and this is going to be my
+                personal Website. Here, you will find everything that I want
+                you to know about me as a person, my hobbies and personal
+                projects.
+            </Block>
+            <Block paragraph>
                 This site is currently under construction! So dont wonder if
                 parts dont feel super snappy, are incomplete or feel empty.
-            </p>
-            <p className="font-bold">
+            </Block>
+            <Block paragraph>
                 Stay Tuned!
-            </p>
-            <CodeSample title="Example Code" src="sample.kt" language="Java" />
+            </Block>
+            <Block>
+                <CodeSample title="Example Code" src="sample.kt" language="Java" />
+            </Block>
         </BasicPage>
     );
 }
--- lausi95/snippets/sample.kt
+++ lausi95/snippets/sample.kt
@@ -1,3 +1,4 @@
+// some demo code to see that the synxtax highlighing works..
 fun main() {
-    println("Hallo, Welt!!")
+    println("Hallo, Welt")
 }
